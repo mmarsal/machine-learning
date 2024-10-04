561a92de9481e6f0c5a20b0493c5af0a580d28d4
Martin Marsal <mmarsal@stud.hs-heilbronn.de>
2024-10-04 15:13:06 +0200

    Initial commit

---
+++ naive_bayes_classifier.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+class NaiveBayes:
+    """
+    Naive Bayes classifier for continuous and discrete features using pandas
+    """
+
+    def __init__(self, continuous=None):
+        """
+        :param continuous: list containing a bool for each feature column to be analyzed. True if the feature column
+                           contains a continuous feature, False if discrete
+        """
+        pass
+
+    def fit(self, data: pd.DataFrame, target_name: str):
+        """
+        Fitting the training data by saving all relevant conditional probabilities for discrete values or for continuous
+        features.
+        :param data: pd.DataFrame containing training data (including the label column)
+        :param target_name: str Name of the label column in data
+        """
+        pass
+
+    def predict_probability(self, data: pd.DataFrame):
+        """
+        Calculates the Naive Bayes prediction for a whole pd.DataFrame.
+        :param data: pd.DataFrame to be predicted
+        :return: pd.DataFrame containing probabilities for all categories as well as the classification result
+        """
+        pass
+
+    def evaluate_on_data(self, data: pd.DataFrame, test_labels):
+        """
+        Predicts a test DataFrame and compares it to the given test_labels.
+        :param data: pd.DataFrame containing the test data
+        :param test_labels:
+        :return: tuple of overall accuracy and confusion matrix values
+        """
+        pass
