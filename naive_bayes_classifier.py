9039d9e27ec00fac4112d4560707249f2494b8f3
Martin Marsal <mmarsal@stud.hs-heilbronn.de>
2024-10-08 17:08:03 +0200

    Implement fit function

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 class NaiveBayes:
     """
     Naive Bayes classifier for continuous and discrete features using pandas
@@ -12,6 +11,9 @@ class NaiveBayes:
                            contains a continuous feature, False if discrete
         """
         self.continuous = continuous
+        self.priors = {} # stores prior probabilities
+        self.likelihoods = {} # stores likelihoods for discrete features
+        self.gaussian_parameters = {} # stores gaussian parameters for continuous features
 
     def fit(self, data: pd.DataFrame, target_name: str):
         """
@@ -20,7 +22,36 @@ class NaiveBayes:
         :param data: pd.DataFrame containing training data (including the label column)
         :param target_name: str Name of the label column in data
         """
-        pass
+        # Split the training data into two groups (disease/no disease)
+        grouped = data.groupby(target_name)
+        no_disease_group = grouped.get_group(False)
+        disease_group = grouped.get_group(True)
+
+        # Calculate prior probabilities
+        total_count = len(data)
+        self.priors['disease'] = len(disease_group) / total_count
+        self.priors['no_disease'] = len(no_disease_group) / total_count
+
+        # Drop label column and setup parameters
+        features = data.columns.drop(target_name)
+        self.likelihoods = {'disease': {}, 'no_disease': {}}
+        self.gaussian_parameters = {'disease': {}, 'no_disease': {}}
+
+        # Calculate likelihoods and gaussian parameters
+        for i, feature in enumerate(features):
+            if self.continuous[i]: # If continuous feature
+                self.gaussian_parameters['disease'][feature] = {
+                    'mean': disease_group[feature].mean(),
+                    'std': disease_group[feature].std()
+                }
+                self.gaussian_parameters['no_disease'][feature] = {
+                    'mean': no_disease_group[feature].mean(),
+                    'std': no_disease_group[feature].std()
+                }
+            else: # If discrete feature
+                self.likelihoods['disease'][feature] = disease_group[feature].value_counts(normalize=True).to_dict()
+                self.likelihoods['no_disease'][feature] = no_disease_group[feature].value_counts(
+                    normalize=True).to_dict()
 
     def predict_probability(self, data: pd.DataFrame):
         """
